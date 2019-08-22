34aedbe00b9c22dddea1d6f7ffd28e5696f0198d
reade <reade@helpshift.com>
2019-08-22 14:44:45 -0700

    iOS Config

--- ios/RNHelpshift.m
+++ ios/RNHelpshift.m
@@ -1,5 +1,9 @@
 
 #import "RNHelpshift.h"
+#import "RCTLog.h"
+
+#import "HelpshiftCore.h"
+#import "HelpshiftSupport.h"
 
 @implementation RNHelpshift
 
@@ -8,6 +12,24 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(init:(NSString *)apiKey domain:(NSString *)domain appId:(NSString *)appId)
+{
+    [HelpshiftCore initializeWithProvider:[HelpshiftSupport sharedInstance]];
+    [HelpshiftCore installForApiKey:apiKey domainName:domain appID:appId];   
+}
+
+RCT_EXPORT_METHOD(showConversation)
+{
+  UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+  [HelpshiftSupport showConversation:rootController withOptions:nil];
+}
+
+RCT_EXPORT_METHOD(showFAQs)
+{
+  UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+  [HelpshiftSupport showFAQs:rootController withOptions:nil];
+}
 
 @end
   
