b0b00208bf5dfa825f133d199d5d884102d916aa
reade <reade@helpshift.com>
2019-08-23 10:43:55 -0700

    Android Config

--- android/src/main/java/com/reactlibrary/RNHelpshiftModule.java
+++ android/src/main/java/com/reactlibrary/RNHelpshiftModule.java
@@ -4,15 +4,41 @@ package com.reactlibrary;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import com.helpshift.Core;
+import com.helpshift.All;
+import com.helpshift.InstallConfig;
+import com.helpshift.exceptions.InstallException;
+import com.helpshift.support.Support;
+
+import android.app.Activity;
+import android.app.Application;
 
 public class RNHelpshiftModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
-
+  private final Application app;
+  
   public RNHelpshiftModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.reactContext = reactContext;
+    this.app = (Application)reactContext.getApplicationContext();
+  }
+
+  @ReactMethod
+  public void init(String key, String domain, String appid) throws InstallException {
+      Core.init(All.getInstance());
+      Core.install(this.app, key, domain, appid);
+  }
+
+  @ReactMethod
+  public void showConversation(){
+    final Activity activity = getCurrentActivity();
+    Support.showConversation(activity);
+  }
+
+  @ReactMethod
+  public void showFAQs(){
+    final Activity activity = getCurrentActivity();
+    Support.showFAQs(activity);
   }
 
   @Override
