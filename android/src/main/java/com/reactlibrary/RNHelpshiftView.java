3c93d49fef86df6b4ce53b048ba407c8a79685ea
readehelpshift <reade@helpshift.com>
2019-10-01 12:29:04 -0700

    Add delegate methods in order to add sessionEnded event

--- android/src/main/java/com/reactlibrary/RNHelpshiftView.java
+++ android/src/main/java/com/reactlibrary/RNHelpshiftView.java
@@ -2,28 +2,36 @@ package com.reactlibrary;
 
 import android.app.Application;
 import android.graphics.Color;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.helpshift.Core;
 import com.helpshift.HelpshiftUser;
+import com.helpshift.delegate.AuthenticationFailureReason;
 import com.helpshift.exceptions.InstallException;
 import com.helpshift.support.ApiConfig;
 import com.helpshift.support.Support;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class RNHelpshiftView extends SimpleViewManager<FrameLayout> {
+public class RNHelpshiftView extends SimpleViewManager<FrameLayout> implements Support.Delegate {
 
     public static final String REACT_CLASS = "RNTHelpshift";
 
@@ -48,6 +56,7 @@ public class RNHelpshiftView extends SimpleViewManager<FrameLayout> {
 
     @ReactProp(name = "config")
     public void setConfig(FrameLayout frameLayout, ReadableMap config) throws InstallException {
+        Support.setDelegate(this);
         Core.init(Support.getInstance());
         Core.install(mApplication,  config.getString("apiKey"),  config.getString("domain"),  config.getString("appId"));
 
@@ -75,22 +84,22 @@ public class RNHelpshiftView extends SimpleViewManager<FrameLayout> {
     private void login(ReadableMap user){
         HelpshiftUser userBuilder;
         String email = user.hasKey("email") ? user.getString("email") : null;
-        String identifier = user.hasKey("identifier") ? user.getString("identifier") : null;
+        String indentifier = user.hasKey("indentifier") ? user.getString("indentifier") : null;
         if(user.hasKey("name") && user.hasKey("authToken")) {
-            userBuilder = new HelpshiftUser.Builder(identifier, email)
+            userBuilder = new HelpshiftUser.Builder(indentifier, email)
                     .setName(user.getString("name"))
                     .setAuthToken(user.getString("authToken"))
                     .build();
         } else if (user.hasKey("name")) {
-            userBuilder = new HelpshiftUser.Builder(identifier, email)
+            userBuilder = new HelpshiftUser.Builder(indentifier, email)
                     .setName(user.getString("name"))
                     .build();
         } else if (user.hasKey("authToken")) {
-            userBuilder = new HelpshiftUser.Builder(identifier, email)
+            userBuilder = new HelpshiftUser.Builder(indentifier, email)
                     .setAuthToken(user.getString("authToken"))
                     .build();
         } else {
-            userBuilder = new HelpshiftUser.Builder(identifier, email).build();
+            userBuilder = new HelpshiftUser.Builder(indentifier, email).build();
         }
 
         Core.login(userBuilder);
@@ -107,6 +116,57 @@ public class RNHelpshiftView extends SimpleViewManager<FrameLayout> {
         }
 
         return customIssueFields;
+    }
+
+    private void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    @Override
+    public void sessionBegan() {
+      Log.d("Helpshift", "sessionBegan");
+    }
+
+    @Override
+    public void sessionEnded() {
+      Log.d("Helpshift", "sessionEnded");
+      sendEvent(mReactContext, "helpshiftSessionEnded", Arguments.createMap());
+    }
+
+    @Override
+    public void newConversationStarted(String newConversationMessage) {
+      Log.d("Helpshift", "newConversationStarted");
+    }
+
+    @Override
+    public void conversationEnded() {
+      Log.d("Helpshift", "conversationEnded");
+    }
+
+    @Override
+    public void userRepliedToConversation(String newMessage) {
+      Log.d("Helpshift", "userRepliedToConversation");
+    }
+
+    @Override
+    public void userCompletedCustomerSatisfactionSurvey(int rating, String feedback) {
+      Log.d("Helpshift", "userCompletedCustomerSatisfactionSurvey");
+    }
+
+    @Override
+    public void displayAttachmentFile(File attachmentFile) {
+      Log.d("Helpshift", "displayAttachmentFile");
+    }
+
+    @Override
+    public void didReceiveNotification(int newMessagesCount) {
+      Log.d("Helpshift", "didReceiveNotification");
+    }
+
+    @Override
+    public void authenticationFailed(HelpshiftUser user, AuthenticationFailureReason reason) {
+      Log.d("Helpshift", "authenticationFailed");
     }
 
 }
