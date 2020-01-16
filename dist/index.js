b8abefaa2bef87ccd91e8a9c87d6a8240004d4b2
Paul Michalak <paul@brex.com>
2020-01-16 14:21:40 -0800

    Type and fix library

---
+++ dist/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { NativeEventEmitter, NativeModules, requireNativeComponent, View } from "react-native";
+function getHelp() {
+    try {
+        const loaded = requireNativeComponent("RNTHelpshift");
+        return loaded;
+    }
+    catch (_a) {
+        return View;
+    }
+}
+const RNTHelpshift = getHelp();
+const { RNHelpshift } = NativeModules;
+const mockEmitter = {
+    once: () => null,
+    addListener: () => null,
+    removeListener: () => null,
+    removeAllListeners: () => null
+};
+const MODULE_UNAVAILABLE_WARNING = "Native module unavailable in project binary.";
+class Helpshift extends React.PureComponent {
+    render() {
+        return <RNTHelpshift {...this.props}/>;
+    }
+}
+Helpshift.init = (apiKey, domain, appId) => RNHelpshift
+    ? RNHelpshift.init(apiKey, domain, appId)
+    : console.warn(MODULE_UNAVAILABLE_WARNING);
+// TODO: Rerender Helpshift view on iOS if using <Helpshift/>
+Helpshift.login = (user) => RNHelpshift
+    ? RNHelpshift.login(user)
+    : console.warn(MODULE_UNAVAILABLE_WARNING);
+Helpshift.logout = () => RNHelpshift
+    ? RNHelpshift.logout()
+    : console.warn(MODULE_UNAVAILABLE_WARNING);
+Helpshift.showFAQs = () => RNHelpshift
+    ? RNHelpshift.showFAQs()
+    : console.warn(MODULE_UNAVAILABLE_WARNING);
+Helpshift.requestUnreadMessagesCount = () => RNHelpshift
+    ? RNHelpshift.requestUnreadMessagesCount()
+    : console.warn(MODULE_UNAVAILABLE_WARNING);
+Helpshift.eventEmitter = RNHelpshift
+    ? new NativeEventEmitter(RNHelpshift)
+    : mockEmitter;
+export default Helpshift;
